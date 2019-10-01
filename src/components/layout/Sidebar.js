8e5dd844ca3ac12348f4d7798962c96ec8db23d8
damienteo <ksteo89@gmail.com>
2019-10-01 23:34:36 +0800

    Populate sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,5 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+
+import {
+  faChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+  FaChevronDown
+} from "react-icons/fa";
 
 export const Sidebar = () => {
-  return <p>Sidebar</p>;
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar_generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className="sidebar_middle">
+        <span>
+          <FaChevronDown />
+          <h2>Project</h2>
+        </span>
+      </div>
+      <ul className="sidebar_projects">
+        <li>Projects will be here</li>
+      </ul>
+      Add Project!
+    </div>
+  );
 };
