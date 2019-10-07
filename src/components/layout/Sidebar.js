17888051976f6b9ff1f67642c199c605a67f1dc2
damienteo <ksteo89@gmail.com>
2019-10-07 17:44:54 +0800

    Some styling for sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -11,33 +11,33 @@ import {
 export const Sidebar = () => {
   return (
     <div className="sidebar" data-testid="sidebar">
-      <ul className="sidebar_generic">
-        <li>
+      <ul className="sidebar__generic">
+        <li data-testid="inbox" className="inbox">
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li>
+        <li data-testid="today" className="today">
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li>
+        <li data-testid="next_7" className="next_7">
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-      <div className="sidebar_middle">
+      <div className="sidebar__middle">
         <span>
           <FaChevronDown />
           <h2>Project</h2>
         </span>
       </div>
-      <ul className="sidebar_projects">
+      <ul className="sidebar__projects">
         <li>Projects will be here</li>
       </ul>
       Add Project!
