311ae6328ff7796dd54a271315b6af892ddab55c
damienteo <ksteo89@gmail.com>
2019-10-08 11:20:06 +0800

    Add styling to tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -36,7 +36,7 @@ export const Tasks = () => {
 
   return (
     <div className="tasks" data-testid="tasks">
-      <h2 data-testid="project-name">Tasks</h2>
+      <h2 data-testid="project-name">{projectName}</h2>
       <ul className="tasks_list">
         {tasks.map(task => (
           <li key={`${task.id}`}>
