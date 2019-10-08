82a6520075811e12db61343a52d0fb2e9449a44d
damienteo <ksteo89@gmail.com>
2019-10-08 14:29:40 +0800

    Add content to addProject

--- src/components/layout/AddProject.js
+++ src/components/layout/AddProject.js
@@ -1,5 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const AddProject = () => {
-  return <p>Add project!</p>;
+import { firebase } from "../../firebase";
+import { generatePushId } from "../../helpers";
+import { useProjectsValue } from "../../context";
+
+export const AddProject = ({ shouldShow = false }) => {
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState("");
+
+  const projectId = generatePushId();
+  const { setProjects } = useProjectsValue();
+
+  const addProject = () => {
+    projectName &&
+      firebase.firestore().collection(
+        "projects"
+          .add({
+            projectId,
+            name: projectName,
+            userId: "DtKDdYFy"
+          })
+          .then(() => {
+            setProjects([]);
+            setProjectName("");
+            setShow(false);
+          })
+      );
+  };
+
+  return (
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input">
+          <input
+            value={projectName}
+            onChange={e => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+          />
+          <button
+            className="'add-project__submit"
+            type="button"
+            onClick={() => addProject()}
+            data-testid="add-project-submit"
+          >
+            Add Project
+          </button>
+          <span
+            data-testid="hide-project-overlay"
+            className="add-project__cancel"
+            onClick={() => setShow(false)}
+          >
+            Cancel
+          </span>
+        </div>
+      )}
+      <span className="add-project__plus">+</span>
+      <span
+        data-testid="add-project-action"
+        classname="add-project__test"
+        onClick={() => setShow(!show)}
+      >
+        Add project
+      </span>
+    </div>
+  );
 };
